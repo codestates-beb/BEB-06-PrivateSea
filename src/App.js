5703b89c02d7e4a40c75fa2f6dddfd8782fcb427
kimnahye <nahye7670@gmail.com>
2022-10-19 17:06:17 +0900

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Nav from "./Nav.js";
 
 function App() {
-  return <div>main</div>;
+  return (
+    <div>
+      <Nav />
+      123
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <input type="text" placeholder="search"></input>
+      <button>Explore</button>
+      <button>Create</button>
+      <button>Mypage</button>
+      <button>Wallet</button>
+    </div>
+  );
+}
+
+export default Nav();
