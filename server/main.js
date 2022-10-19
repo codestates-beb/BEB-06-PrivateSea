b835e6efe86fd1a7436cc12eb47029998d583d51
kimnahye <nahye7670@gmail.com>
2022-10-19 16:40:29 +0900

    first

---
+++ server/main.js
@@ -0,0 +1,2 @@
+//req.prams.id 를 filter 처리한다. (account, tokenId, collectionName)
+//데이터베이스에서 받은 정보를 프론트로 보내준다.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <div>main</div>;
+}
+
+export default App;
