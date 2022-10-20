f853586b4fb1c61af0ec028cecd69d409df8bf3d
김륜하 <ryoonzz@gmail.com>
2022-10-20 20:42:44 +0900

    로직완성

--- src/page/Create.js
+++ src/page/Create.js
@@ -2,6 +2,20 @@ import React from "react";
 import Nav from "../Nav";
 function Create() {
   return (
+
+/** 
+1. 사용자가 nft 생성을 위해 필요한 정보 -> input 처리 
+image 클릭하면 -> desktop 이나 image upload 
+item name(nft name)
+discription
+collection 
+theme: 3가지 옵션 중 하나를 선택하도록 - art, sport, photo 
+
+2. 서버에 보내줘야 하는 값
+1번의 정보 + account값
+
+ */
+
     <div>
       <Nav />
       Create Page
--- src/page/Mypage.js
+++ src/page/Mypage.js
@@ -2,6 +2,21 @@ import React from "react";
 import Nav from "../Nav";
 
 function Mypage() {
+
+/**
+1. 지갑연결 
+연결 X -> 주소란에 , 사이트창에 "아직 연결 안 됐습니다" 알림
+연결 O -> 주소란에 자기 acount 보임, 사이트창에 자기가 소유한 nft가 보임 
+
+2. 소유한 nft에 sell 버튼 달기 
+
+3. 서버에 값 보내고 정보 받기; 간단하게 보이는 화면 -> 개별 nft 누르면 Detail.js로 이동  
+  - nft url
+  - nft name
+  - nft 가격
+
+  4. 서버에 sell 요청 보내면 "(임의)판매가 완료되었습니다~" 등의 문구가 뜨도록 <- 문구는 백에서 알아서 
+ */
   return (
     <div>
       <Nav />
