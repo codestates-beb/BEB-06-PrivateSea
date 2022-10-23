801e4f8aa23cc43e6bbfbffdaa3f3d3f72fb8d03
jungbal <terykid@naver.com>
2022-10-23 19:05:30 +0900

    fix server

--- server/main.js
+++ server/main.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const path = require('path');
+const test = require("./reactrouter.js")
 const cors = require('cors');
 app.use(cors());
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-let db;
+var db;
 MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true }, function(err, client){
     if(err){
         return console.log(err)
     }
+   
+    app.get('/nft/:theme', function(req, res){
+        db = client.db('project1');
+        db.collection('img_table').find({theme : req.params.theme}).toArray(function(err, result){
+            if(err) return console.log(err);
+            console.log(result);
+            res.json({result : result});
+        })
+    })
+
+    app.patch('/nft/:tokenid/:owner', function(req, res){
+        db = client.db('project1');
+        console.log(req.params)
+        db.collection('img_table').findOneAndUpdate({tokenid : req.params.tokenid}, {$set : {owner : req.params.owner}}, function(err, result){
+            if(err) return console.log(err)
+            console.log(result)
+            res.json({result :  result});
+        })
+    })
+
     app.get('/nft', function(req, res){
         db = client.db('project1');
         console.log(req.query.value)
@@ -21,24 +41,25 @@ MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true }, func
                     index: 'nft_search',
                     text: {
                         query: req.query.value,
-                        path: ['account', 'tokenId', 'collectionName'],
+                        path: ['owner', 'tokenid', 'name'],
                     }
                 }
             },
-            { $sort : {title : 1}}
+            { $sort : {name : 1}}
         ]
         db.collection('img_table').aggregate(searchCon).toArray(function(err, result){
             if(err) return console.log(err)
             console.log(result)
-            res.render('검색된 페이지', {img : result})
+            res.json({result : result});
         })
     })
 
-    app.use(express.static(path.join(__dirname, '../build')))
+
+    // app.use(express.static(path.join(__dirname, '../build')))
     
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, '../build/index.html'))
-    })
+    // app.get('*', function(req, res){
+    //     res.sendFile(path.join(__dirname, '../build/index.html'))
+    // })
 
     app.listen(8080, function(){
         console.log('listening on 8080')
