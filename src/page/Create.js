3ee456560376e51128dbec6f183b543fdc57097e
김륜하 <ryoonzz@gmail.com>
2022-10-25 08:40:55 +0900

    feat:변경사항없음

--- src/page/Create.js
+++ src/page/Create.js
@@ -12,45 +12,45 @@ function Create(){
   const [tokenId, setTokenId] = useState();
   const [theme, setTheme] = useState(['art', 'sport', 'photo'])
   
-  const handleText = (e) => {
-    setText(e.target.value);
-  }
+  // const handleText = (e) => {
+  //   setText(e.target.value);
+  // }
 
-  const uploadImage = (e) => {
-    setImage(e.target.value);
-  }
+  // const uploadImage = (e) => {
+  //   setImage(e.target.value);
+  // }
 
-  const onRest = () => {
-    setText("");
-  }
+  // const onRest = () => {
+  //   setText("");
+  // }
 
   // DB에 nft 생성 데이터 post 요청
-  async function createNft(){
-    await axios.post ("https://testnets-api.opensea.io/api/v1/assets?owner=0xEcd5c913FC8B656dbfe0f2d902E1b0902de025aA&order_direction=desc&offset=0&limit=20&include_orders=false%27", {
-      name: {name},
-      image: {image},
-      price: {price},
-      theme: {theme},
-      tokenId: tokenId,
-      description: {desc}
-    }).then(function(response){
-      let data = {
-        name: {name},
-        image: {image},
-        price: {price},
-        theme: {theme},
-        tokenId: tokenId,
-        description: {desc}
-      }
-      let message = "Success 🎉";
-      alert(response.data.message);
-      console.log(response);
+  // async function createNft(){
+  //   await axios.post ("http://localhost:8080/nft/", {
+  //     name: {name},
+  //     image: {image},
+  //     price: {price},
+  //     theme: {theme},
+  //     tokenId: tokenId,
+  //     description: {desc}
+  //   }).then(function(response){
+  //     let data = {
+  //       name: {name},
+  //       image: {image},
+  //       price: {price},
+  //       theme: {theme},
+  //       tokenId: tokenId,
+  //       description: {desc}
+  //     }
+  //     let message = "Success 🎉";
+  //     alert(response.data.message);
+  //     console.log(response);
       
-    }).catch(function(err){
-      alert("Failed");
-      console.log(err);
-    })
-  }
+  //   }).catch(function(err){
+  //     alert("Failed");
+  //     console.log(err);
+  //   })
+  // }
 
 
   return(
@@ -61,31 +61,24 @@ function Create(){
       <div>
 
         {/* 생성할 nft 정보 */}
-        <div className="imageFile">
-          <input type="file" onClick={ uploadImage(image)}/>
-        </div>
-        <div input type="text" placeholder="NFT Name" onChange={(e) => handleText(e)} value={name} />
-        <div input type="text" placeholder="Price" onChange={(e) => handleText(e)} value={price} />
-        <textarea placeholder="Description" onChange={(e) => handleText(e)} value={desc} />
+        <div className="imageFile" />
+          {/* <input type="file" /> */}
+        {/* </div> */}
+        <input type="text" placeholder="Name"  />
+        <input type="text" placeholder="Price"   />
+        <textarea placeholder="Description"   />
 
 
-        {/* 테마설정 어떻게 해야 할지 모르겠어요 ㅠㅠ */}
-        <div onMouseEnter={ setTheme()}>
-          {
-            theme.map(function(a){
-              <div> {theme} </div>
-            })
-          }
-      </div>
+
 
       {/* 생성 및 초기화 버튼
         create 제출 -> nft 생성 및 db post 요청 */}
       </div>
       <div>
-        <button onSubmit={ createNft()} style={{color: blue}}> Create </button>
+        <button  style={{color: "blue"}}> Create </button>
       </div>
       <div>
-        <button onClick={ onRest()} style={{color: blue}}> Reset </button>
+        <button  style={{color: "blue"}}> Reset </button>
       </div>
 
     </>
